3312f56fa395990d57e39166de71c6a190694fb8
Marco <marcodomat@gmail.com>
2021-01-22 14:47:12 +0100

    k

--- wordpress/index.php
+++ wordpress/index.php
@@ -3,4 +3,13 @@
     
     get_footer();
 
+    /*
+    <?php 
+    if ( have_posts() ) : 
+        while ( have_posts() ) : the_post(); 
+            // Display post content
+        endwhile; 
+    endif; 
+    ?>
+    */
 ?>
